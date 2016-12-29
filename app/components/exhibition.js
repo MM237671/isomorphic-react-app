c0216b709c524fd4465bfc2e3aac32b9e12aba8e
anton.s.pilipenko <anton.s.pilipenko@gmail.com>
2016-12-29 12:08:08 +0300

    fix

--- app/components/exhibition.js
+++ app/components/exhibition.js
@@ -11,23 +11,28 @@ class Exhibition extends Component {
           return (
             <div key={key} className="wrapBlock exhibitionListItem">
               <h1>{item.name}<br /><small>{item.description}</small></h1>
+              {item.photo != undefined && (
+
+                <img className="TextWrapLeft" width="200" src={`${config.staticUrl}${item.photo.src}`} alt={item.photo.alt} title={item.photo.title} />
+              )}
               <address className="address">
                 <span>C {item.from}</span>&nbsp;по&nbsp;<span>{item.to}</span><br />
                 <span>{item.address}</span>
               </address>
               <ul>
-                <li>Участники: {item.artists}</li>
-                <li>Техники: {item.materials}</li>
-                <li>Фото некоторых работ:
-                  <div>
-                    {item.photos.map((p, id) => {
-                      return (
-                        <img height="100px" key={id} src={`${config.staticUrl}${p.src}`} alt={p.alt} title={p.title} />
-                      );
-                    })}
-                  </div>
-                </li>
+                <li><i>Участники:</i> {item.artists}</li>
+                <li><i>Техники:</i> {item.materials}</li>
               </ul>
+              <div className="clear"></div>
+              <div><i>Фото некоторых работ:</i>
+                <div>
+                  {item.photos.map((p, id) => {
+                    return (
+                      <img height="100px" key={id} src={`${config.staticUrl}${p.src}`} alt={p.alt} title={p.title} />
+                    );
+                  })}
+                </div>
+              </div>
             </div>
           );
         })}
