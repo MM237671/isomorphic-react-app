8cb457dde99d1cc0b839cb598ba846004ee933bc
anton.s.pilipenko <anton.s.pilipenko@gmail.com>
2016-12-29 13:57:56 +0300

    fix

--- app/components/exhibition.js
+++ app/components/exhibition.js
@@ -28,7 +28,7 @@ class Exhibition extends Component {
                 <div>
                   {item.photos.map((p, id) => {
                     return (
-                      <img height="100px" key={id} src={`${config.staticUrl}${p.src}`} alt={p.alt} title={p.title} />
+                      <img className="imageWithBorder" height="200px" key={id} src={`${config.staticUrl}${p.src}`} alt={p.title} title={p.title} />
                     );
                   })}
                 </div>
