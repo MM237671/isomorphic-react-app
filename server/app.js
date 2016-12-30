28b19babf59a531e39ee1b1158d03a81ed8d6b28
anton.s.pilipenko <anton.s.pilipenko@gmail.com>
2016-12-30 15:01:53 +0300

    sitemap

--- server/app.js
+++ server/app.js
@@ -9,9 +9,121 @@ import routes from '../app/routes.js';
 import configureStore from '../app/store/configureStore';
 import config from '../config/index';
 
-
+const sm = require('sitemap');
 const initValues = require('./exhibitions.json');
 const app = express();
+
+
+const sitemap = sm.createSitemap({
+  hostname: 'http://remesel.ru',
+  cacheTime: 600000,
+  urls: [
+    { url: '/' },
+    { url: '/exhibition/polenovo2016-2017',
+      img: [
+        {
+          url: '/static/i/enamel.jpg',
+          caption: 'Эмаль, автор - Пилипенко Михаил'
+        },
+        {
+          url: '/static/i/sculpt.jpg',
+          caption: 'Скульптура дерево/тонировка, автор - Пилипенко Сергей'
+        },
+        {
+          url: '/static/i/paint.jpg',
+          caption: 'Живопись холст/масло, автор - Пилипенко Михаил'
+        },
+        {
+          url: '/static/i/batik.jpg',
+          caption: 'Батик, автор - Пилипенко Виктория'
+        }
+      ]
+    },
+    { url: '/artist/PilipenkoTonya',
+      img: [
+        {
+          url: '/works/tonya/Дом у реки_бум,акв_20x27_2008.JPG',
+          caption: 'Дом у реки - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Изумрудные деревья_бум,акв_31х42,2_2012.jpg',
+          caption: 'Изумрудные деревья - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Отражение_бум, акв_19,3х29,5_Суздаль2014.jpg',
+          caption: 'Отражение - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Половодье на Таруске_бум,акв,пастель_25,9х41,2_2010.jpg',
+          caption: 'Половодье на Таруске - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Свежий снег_бум,акв,гуашь_33,3х41_2011.JPG',
+          caption: 'Свежий снег - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Тихий вечер_бум,акв_32х24_Суздаль2011.jpg',
+          caption: 'Тихий вечер - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/Японский натюрморт_бум,акв_2010.jpg',
+          caption: 'Японский натюрморт - бумага/акварель, Пилипенко Антонина'
+        },
+        {
+          url: '/works/tonya/2016-07-03 14-26-08.JPG',
+          caption: 'Белые цветы - бумага/акварель, Пилипенко Антонина'
+        }
+      ]
+    },
+    { url: '/artist/PilipenkoMihail',
+      img: [
+        {
+          url: '/works/miha/IMG_1970.JPG',
+          caption: 'Домик художников И.Старженецкой и А.Комелина, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1971.JPG',
+          caption: 'Снег выпал, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1972.JPG',
+          caption: 'Зимние деревья, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1973.JPG',
+          caption: 'Осеннее утро, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1974.JPG',
+          caption: 'Погода меняется, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1975.JPG',
+          caption: 'Вечереет, холст/масло, Пилипенко Михаил'
+        },
+        {
+          url: '/works/miha/IMG_1976.JPG',
+          caption: 'Первый снег, холст/масло, Пилипенко Михаил'
+        }
+      ]
+    },
+    { url: '/artist/PilipenkoViktoriya' },
+    { url: '/artist/PilipenkoSergey' },
+    { url: '/artist/RostemberskayaGalina' }
+  ]
+});
+
+
+app.get('/sitemap.xml', (req, res) => {
+  sitemap.toXML((err, xml) => {
+    if (err) {
+      return res.status(500).end();
+    }
+    res.header('Content-Type', 'application/xml');
+    res.send(xml);
+  });
+});
+
 
 app.use('/static', express.static('public/static'));
 
@@ -91,23 +203,23 @@ function renderHTML({ componentHTML, initialState, metaInfo, conf }) {
       <!DOCTYPE html>
       <html>
       <head>
-          <meta charset="utf-8">
-          <meta name="viewport" content="width=device-width, initial-scale=1.0">
-          <meta name="description" content="${metaInfo.description}">
-          <meta name="keywords" content="${metaInfo.keywords}">
-          <meta name="author" content="Художники из города Таруса">
-          <meta name="yandex-verification" content="2ac2f3c613df230d" />
-          <link rel="shortcut icon" href="/static/i/cat.png"/>
+          <meta charset='utf-8'>
+          <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+          <meta name='description' content='${metaInfo.description}'>
+          <meta name='keywords' content='${metaInfo.keywords}'>
+          <meta name='author' content='Художники из города Таруса'>
+          <meta name='yandex-verification' content='2ac2f3c613df230d' />
+          <link rel='shortcut icon' href='/static/i/cat.ico'/>
           <title>${metaInfo.title}</title>
-          <link rel="stylesheet" href="${conf.staticHost}/build/main.css">
+          <link rel='stylesheet' href='${conf.staticHost}/build/main.css'>
       </head>
       <body>
-      <div id="app">${componentHTML}</div>
-        <script type="application/javascript">
+      <div id='app'>${componentHTML}</div>
+        <script type='application/javascript'>
           window.__CONFIG__ = ${JSON.stringify(config)};
           window.__INITIAL_STATE__ = ${JSON.stringify(initialState)};
         </script>
-        <script type="application/javascript" src="${conf.staticHost}/build/main.js"></script>
+        <script type='application/javascript' src='${conf.staticHost}/build/main.js'></script>
       </body>
       </html>
   `;
