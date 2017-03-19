4ab0398e240a753381864d9e669c4dd01c34ac5d
anton.s.pilipenko <anton.s.pilipenko@gmail.com>
2017-03-19 20:35:01 +0300

    fix contacts

--- app/components/contacts.js
+++ app/components/contacts.js
@@ -10,10 +10,12 @@ class Contacts extends Component {
         <div className="wrapBlock artistListItemText">
           <h1>{this.loc('Контакты')}</h1><br />
           <dl>
-            <dt>E-mail:</dt>
-            <dd><a href="mailto:medvedb24@yandex.ru">medvedb24@yandex.ru</a></dd>
-            <dt>{this.loc('Пилипенко Михаил')} (instagram):</dt><dd><a href="https://instagram.com/m.remesel.ru" target="_blank">@m.remesel.ru</a></dd>
-            <dt>{this.loc('Пилипенко Сергей')} (instagram):</dt><dd><a href="https://instagram.com/sergey.remesel.ru" target="_blank">@sergey.remesel.ru</a></dd>
+            <dt>{this.loc('Пилипенко Михаил')} (instagram):</dt>
+            <dd>instagram: <a href="https://instagram.com/m.remesel.ru" target="_blank">@m.remesel.ru</a><br />email: <a href="mailto:medvedb24@yandex.ru">medvedb24@yandex.ru</a></dd>
+            <dt>{this.loc('Пилипенко Сергей')}:</dt>
+            <dd>instagram: <a href="https://instagram.com/sergey.remesel.ru" target="_blank">@sergey.remesel.ru</a><br />email: <a href="mailto:sergey.n.pilipenko@gmail.com">sergey.n.pilipenko@gmail.com</a></dd>
+            <dt>{this.loc('Пилипенко Виктория')}:</dt>
+            <dd>instagram: <a href="https://instagram.com/pilipenkobatik" target="_blank">@pilipenkobatik</a><br />email: <a href="mailto:victoria.j.pilipenko@gmail.com">victoria.j.pilipenko@gmail.com</a></dd>
           </dl>
         </div>
       </section>
